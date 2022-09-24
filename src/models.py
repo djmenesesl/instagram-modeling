2daab4ba03a56f1956b47f1f9c8bde15f6bc57ee
djmenesesl <dmenesesl08@gmail.com>
2022-09-24 01:16:11 +0000

    Diagrama realizado

--- src/models.py
+++ src/models.py
@@ -8,7 +8,37 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
+class Personaje(Base):
+    __tablename__ = 'personaje'
+    id = Column(Integer, primary_key=True)
+    name = Column (String(250), nullable=False)
+    gender = Column (String(250), nullable=True)
+    image = Column (String(250), nullable=False)
+    favoritos = relationship("Favorito")
+
+class Planeta(Base):
+    __tablename__ = 'planeta'
+    id = Column(Integer, primary_key=True)
+    name = Column (String(250), nullable=False)
+    image = Column (String(250), nullable=False)
+    favoritos = relationship("Favorito")
+
+class Usuario(Base):
+    __tablename__ = 'usuario'
+    id = Column(Integer, primary_key=True)
+    mail = Column (String(250), nullable=False)
+    password = Column (String(250), nullable=False)
+    favoritos = relationship("Favorito")
+
+class Favorito(Base):
+    __tablename__ = 'favorito'
+    id = Column(Integer, primary_key=True)
+    id_personaje = Column(Integer, ForeignKey('personaje.id'))
+    id_planeta = Column(Integer, ForeignKey('planeta.id'))
+    id_usuario = Column(Integer, ForeignKey('usuario.id'))
+    
+
+"""class Person(Base):
     __tablename__ = 'person'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
@@ -28,6 +58,6 @@ class Address(Base):
 
     def to_dict(self):
         return {}
-
+"""
 ## Draw from SQLAlchemy base
 render_er(Base, 'diagram.png')
